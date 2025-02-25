aedd3d6860c3219fa411e42a3df270605b04de33
chiheb bahri <cheehub.business111@gmail.com>
2025-02-25 11:19:47 +0100

    W2 My first React component

---
+++ first proj/the-first-proj/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import * as React from "react";
+function getTitle(title) {
+ return title;
+}
+function App(){
+  return (
+    <>
+      <div>
+        <h1>Hello {getTitle("react")}</h1>
+        <label htmlFor="search">search:</label>
+        <input id="search" type="text"></input>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
