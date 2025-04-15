e23bb4d36a792ac96769b163a33196a16d3ee7fa
chiheb bahri <cheehub.business111@gmail.com>
2025-04-15 11:26:52 +0100

    arrow and handler functions in react

--- first proj/the-first-proj/src/App.jsx
+++ first proj/the-first-proj/src/App.jsx
@@ -6,17 +6,43 @@ import * as React from "react";
 function getTitle(title) {
  return title;
 }
-function App(){
-  return (
-    <>
-      <div>
-        <h1>Hello {getTitle("react")}</h1>
-        <label htmlFor="search">search:</label>
-        <input id="search" type="text"></input>
-      </div>
-      
-    </>
-  )
-}
+
+const App =() =>{
+  return(<>
+    <h1>My Hacker Stories</h1>
+    <Search />
+    <hr />
+    <List />
+    
+  </>);
+};
+const Search =() =>{
+  const handleChange=(event) =>{
+    console.log(event);
+    console.log(event.target.value);  };
+  return(
+    <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onchange={handleChange}/>
+    </div>
+    );
+};
+const List =() =>{
+  return(<ul>
+    {list.map(function (item) {
+    return (
+    <li key={item.objectID}>
+    <span>
+    <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    </li>
+    );
+    })}
+    </ul>)
+};
+
 
 export default App
