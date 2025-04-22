548c586a0be520325fcf82ea3613aef7226d2026
chiheb bahri <cheehub.business111@gmail.com>
2025-04-22 12:02:54 +0100

    arrow and handler functions in react2

--- first proj/the-first-proj/src/App.jsx
+++ first proj/the-first-proj/src/App.jsx
@@ -1,48 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import * as React from "react";
-function getTitle(title) {
- return title;
+import React, { useState } from 'react';
+
+// Search Component
+function Search({ searchTerm, onSearch }) {
+  console.log('Search renders');
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input
+        id="search"
+        type="text"
+        value={searchTerm}
+        onChange={onSearch}
+      />
+    </div>
+  );
 }
 
-const App =() =>{
-  return(<>
-    <h1>My Hacker Stories</h1>
-    <Search />
-    <hr />
-    <List />
-    
-  </>);
-};
-const Search =() =>{
-  const handleChange=(event) =>{
-    console.log(event);
-    console.log(event.target.value);  };
-  return(
-    <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" onchange={handleChange}/>
-    </div>
-    );
-};
-const List =() =>{
-  return(<ul>
-    {list.map(function (item) {
-    return (
-    <li key={item.objectID}>
-    <span>
-    <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
+// Item Component
+function Item({ item }) {
+  return (
+    <li>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span> {item.author}</span>
+      <span> {item.num_comments}</span>
+      <span> {item.points}</span>
     </li>
-    );
-    })}
-    </ul>)
-};
+  );
+}
 
+// List Component
+function List({ list }) {
+  console.log('List renders');
+  return (
+    <ul>
+      {list.map(item => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  );
+}
 
-export default App
+// App Component
+function App() {
+  console.log('App renders');
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search searchTerm={searchTerm} onSearch={handleSearch} />
+      <List list={filteredStories} />
+    </div>
+  );
+}
+
+export default App;
